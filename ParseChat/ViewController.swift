e7cade7b8f78dbe190e3a9cfcf63015ea80a7e4c
Rohan Trivedi <rohan@Rohans-MacBook-Pro.local>
2017-02-25 13:14:35 -0600

    updates

--- ParseChat/ViewController.swift
+++ ParseChat/ViewController.swift
@@ -7,11 +7,17 @@
 //
 
 import UIKit
+import Parse
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.emailTextField.delegate = self
+        self.passwordTextField.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +25,39 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func loginButtonPressed()
+    {
+        var logins = PFQuery(className: "User")
+        var objects = try! logins.findObjects()
+        for i in objects
+        {
+            var Cusername = i.object(forKey: "username")! as! String
+            var Cpassword = i.object(forKey: "password")! as! String
+            if Cusername == emailTextField.text! && Cpassword == passwordTextField.text!
+            {
+                print("complete")
+            }
+        }
+    }
+    
+    @IBAction func signupButtonPressed()
+    {
+        var logins = PFQuery(className: "User")
+        var objects = try! logins.findObjects()
+        for i in objects
+        {
+            var Cusername = i.object(forKey: "username")! as! String
+            if Cusername == emailTextField.text!
+            {
+                return
+            }
+        }
+        var query = PFObject(className: "User")
+        query.setValue(emailTextField.text!, forKey: "username")
+        query.setValue(passwordTextField.text!, forKey: "password")
+        try! query.save()
+        loginButtonPressed()
+    }
+    
 }
 
